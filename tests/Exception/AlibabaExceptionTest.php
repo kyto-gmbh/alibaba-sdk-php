e8d8cc808c2519f25a5ed1203207cd69c30dca11
Volodymyr Stelmakh <volodymyr.stelmakh@kyto.com>
2022-11-13 22:33:52 +0100

    add alibaba exception test

---
+++ tests/Exception/AlibabaExceptionTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kyto\Alibaba\Tests\Exception;
+
+use Kyto\Alibaba\Exception\AlibabaException;
+use PHPUnit\Framework\TestCase;
+
+class AlibabaExceptionTest extends TestCase
+{
+    public function testConstruct(): void
+    {
+        $message = 'Message';
+        $code = 1;
+        $subMessage = 'Sub-message';
+        $subCode = 'sub.code';
+        $previous = new \RuntimeException('Previous');
+
+        $exception = new AlibabaException($message, $code, $subMessage, $subCode, $previous);
+
+        self::assertSame('Message. Sub-code: "sub.code". Sub-message: "Sub-message".', $exception->getMessage());
+        self::assertSame($code, $exception->getCode());
+        self::assertSame($previous, $exception->getPrevious());
+    }
+}
