5e00d5e5148a7aeb37d7102d91e204f29f9baf2c
Volodymyr Stelmakh <volodymyr.stelmakh@kyto.com>
2022-11-23 13:09:17 +0100

    add get sub message and get sub code to alibaba exception

--- src/Exception/AlibabaException.php
+++ src/Exception/AlibabaException.php
@@ -4,19 +4,29 @@ declare(strict_types=1);
 
 namespace Kyto\Alibaba\Exception;
 
-/**
- * @internal
- */
 class AlibabaException extends \RuntimeException
 {
+    /**
+     * @internal
+     */
     public function __construct(
         string $message,
         int $code,
-        string $subMessage,
-        string $subCode,
+        private string $subMessage,
+        private string $subCode,
         ?\Throwable $previous = null
     ) {
-        $message = sprintf('%s. Sub-code: "%s". Sub-message: "%s".', $message, $subCode, $subMessage);
+        $message = sprintf('%s. Sub-code: "%s". Sub-message: "%s".', $message, $this->subCode, $this->subMessage);
         parent::__construct($message, $code, $previous);
+    }
+
+    public function getSubMessage(): string
+    {
+        return $this->subMessage;
+    }
+
+    public function getSubCode(): string
+    {
+        return $this->subCode;
     }
 }
