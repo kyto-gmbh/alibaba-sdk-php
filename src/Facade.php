6f60630d1ff023f10eaa07abe966e237a78624b8
Volodymyr Stelmakh <volodymyr.stelmakh@kyto.com>
2024-04-30 17:16:32 +0200

    update facade to use new authorization url

--- src/Facade.php
+++ src/Facade.php
@@ -19,14 +19,14 @@ class Facade
     /**
      * Facade factory method. Use this to create new Alibaba SDK interaction object.
      *
-     * @param string $apiKey Also referenced as "app key" in the Alibaba docs
-     * @param string $secret Also referenced as "app security" in the Alibaba docs
+     * @param string $key Also referenced as "AppKey" in the Alibaba docs
+     * @param string $secret Also referenced as "App Secret" in the Alibaba docs
      */
-    public static function create(string $apiKey, string $secret): self
+    public static function create(string $key, string $secret): self
     {
         return new self(
-            $apiKey,
-            new Client($apiKey, $secret, HttpClient::create(), new Clock()),
+            $key,
+            new Client($key, $secret, HttpClient::create(), new Clock()),
         );
     }
 
@@ -34,7 +34,7 @@ class Facade
      * @internal
      */
     public function __construct(
-        private string $apiKey,
+        private string $key,
         private Client $client,
     ) {
         $this->category = CategoryEndpoint::create($this->client);
@@ -45,19 +45,17 @@ class Facade
     /**
      * Making GET request to this URL will ask to login to Alibaba and authorize this API key to have access
      * to the account. In other words client should visit this url and authorize App to access Alibaba account by API.
-     * @link https://developer.alibaba.com/en/doc.htm?spm=a219a.7629140.0.0.188675fe5JPvEa#?docType=1&docId=118416
+     * @link https://openapi.alibaba.com/doc/doc.htm?spm=a2o9m.11193494.0.0.50dd3a3armsNgS#/?docId=56
      *
-     * @param string $callbackUrl URL where authorization code returned. Via method GET in "code" parameter.
+     * @param string $callbackUrl URL where authorization code returned. Via method GET in "code" parameter. Should be
+     *                            the same as in the App settings in Alibaba.
      */
     public function getAuthorizationUrl(string $callbackUrl): string
     {
-        return 'https://oauth.alibaba.com/authorize?' . http_build_query([
+        return 'https://openapi-auth.alibaba.com/oauth/authorize?' . http_build_query([
             'response_type' => 'code',
-            'client_id' => $this->apiKey,
             'redirect_uri' => $callbackUrl,
-            'State' => '1212',
-            'view' => 'web',
-            'sp' => 'ICBU',
+            'client_id' => $this->key,
         ]);
     }
 }
